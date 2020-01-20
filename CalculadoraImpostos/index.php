5caad48fd88820dd65f382b91826e817b591b780
Alvez <joaor4e@gmail.com>
2020-01-20 15:44:28 -0300

    feito exercicio de calculadora de impostos

--- CalculadoraImpostos/index.php
+++ CalculadoraImpostos/index.php
@@ -1,11 +1,3 @@
-<?php
-if (isset($_POST['numero']) && $_POST['numero'] != 0) {
-  $imposto = intval($_POST['imposto']) / 100;
-  $impostocobrado = intval($_POST['numero']) * $imposto;
-  $conta = $_POST['numero'] + $impostocobrado;
-  echo "o total cobrado é: " . $conta;
-}
-?>
 <h1>Calculadora de impostos</h1>
 <form method="POST">
   valor: <br>
@@ -16,3 +8,17 @@ if (isset($_POST['numero']) && $_POST['numero'] != 0) {
   <br>
   <input type="submit" value="enviar">
 </form>
+<?php
+if (isset($_POST['numero']) && $_POST['numero'] != 0) {
+  $imposto = intval($_POST['imposto']) / 100;
+  $impostocobrado = intval($_POST['numero']) * $imposto;
+  $conta = $_POST['numero'] - $impostocobrado;
+  echo "o valor inicial é: " . $_POST['numero'];
+  echo "<br>";
+  echo "o imposto é de: " . $_POST['imposto'] . "%";
+  echo "<hr>";
+  echo "o imposto é: " . $impostocobrado;
+  echo "<br>";
+  echo "o valor do produto é: " . $conta;
+}
+?>
